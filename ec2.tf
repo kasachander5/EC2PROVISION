4b044cd98c1aeab7e7005b83d63e5a8a3c286c32
kasachander5 <145572316+kasachander5@users.noreply.github.com>
2024-01-13 22:46:41 +0800

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,3 +6,15 @@ terraform {
     }
   }
 }
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0120e0e7231daa18b"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
